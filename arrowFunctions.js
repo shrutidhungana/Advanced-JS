3d8cd6b3a85290c7ec496292a1387a26e5284c17
shrutidhungana <shrutidhungana@prixa.net>
2022-12-02 10:35:17 +0545

    Arrow Function

---
+++ arrowFunctions.js
@@ -0,0 +1,33 @@
+// Normal Function
+let welcome = function (name) {
+    return `Hello & Welcome ${name}`
+}
+welcome();
+
+document.write(welcome("Shruti "))
+
+// Arrow Function
+
+let welcome1 = (surname) => {
+    return `${surname}<br>`
+}
+
+welcome()
+
+document.write(welcome1("Dhungana"))
+
+/*If there is single line of code in arrow function you don't need return keyword and curly brackets. */
+
+let welcome2 = (name, age) => `The name and age is ${name} and ${age}<br>`
+
+welcome2()
+
+document.write(welcome2("Shruti", 22))
+
+// If you have only one argument to pass in the function you don't need parenthesis.
+
+let welcome3 = name => `The name is ${name}`
+
+welcome3()
+
+document.write(welcome3("Shruti"))
--- templateliteral.js
+++ templateliteral.js
@@ -1,2 +1,22 @@
 let user = "Shruti Dhungana"
-document.write("Hello " + user)
+document.write("Hello " + user + "<br>") //This is concatination 
+document.write(`Hello ${user} <br>`) //This is template literal
+ 
+var y = "10"
+var x = "20"
+let print = `"${y}"<br>${x}<br>`
+document.write(print)
+let users = "Ram"
+let marks = 350;
+let result = `Hello ${user} you have obtained ${marks}<br>`
+document.write(result)
+
+let firstName = "Shruti"
+let lastName = "Dhungana"
+
+function fullName(firstName, lastName) {
+    return `${firstName} ${lastName}<br>`
+}
+
+let hello = `${fullName(firstName, lastName)}`
+document.write(hello)
